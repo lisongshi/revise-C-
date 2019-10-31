7b333df2c4dd354b2714ab1f35e7bb0f8adcbb0b
lisongshi <lisong.shi@whu.edu.cn>
2019-10-31 20:41:15 +0800

    Update vectorFunc.h

--- reviseSTL/vectorFunc.h
+++ reviseSTL/vectorFunc.h
@@ -34,7 +34,56 @@ template<typename T>
 bool insertVector (std::vector<T> & list, int index)
 {
 	std::vector<T> temp = { 2.5, 6.8, 0.3 };
-	auto Iter = list.insert (list.begin () + index, temp.begin (), temp.end ());
+	list.insert (list.begin () + index, temp.begin (), temp.end ());
+	//  <=> list.insert (std::begin (list) + index, std::begin(temp), std::end(temp));
 	showVector (list);
+
+
+	// find all 9 and insert 8 behind them
+	/*
+
+	采取如下写法，由于长度改变，vector 可能会重新分配内存，从而会导致if判断中的迭代器end()失效。
+	要解决问题1.要么一开始给vector设置足够大的内存空间，2.要么在循环 内部！ 修复已经失效的迭代器。
+	同时应当注意当长度改变时不能使用++ -- 等写法，道理同上。
+	另外，find insert返回的都是找到的值以及插入的当前值位置。
+	由于 8 插入 9 的前面因此迭代器指向 8 要跳过当前的 9 ， 迭代器需要 + 2。
+	
+	auto tmpIter = list.begin ();
+	while ( tmpIter != list.end() )
+	{
+		tmpIter = std::find (tmpIter, std::end(list), 9.0);
+		tmpIter = list.insert (tmpIter, 8) + 2;	
+	}
+	*/
+
+	auto tmpIter = list.begin ();
+	auto endFlag = list.end ();
+	while ( tmpIter != endFlag )
+	{
+		tmpIter = std::find (tmpIter, list.end(), 9.0);
+		tmpIter = list.insert (tmpIter, 8) + 2;
+		endFlag = list.end ();
+	}
+	showVector (list);
+
+	// find all 9 and insert 10 after them
+
+	auto tmpIter2 = list.begin ();
+	auto endFlag2 = list.end ();
+	while ( true )
+	{
+		tmpIter2 = std::find (tmpIter2, list.end (), 9.0);
+		if ( tmpIter2 == endFlag2 )  
+			break;
+		else 
+		{
+			tmpIter2 = list.insert (tmpIter2 + 1, 10);
+			endFlag2 = std::end (list);
+		}
+		
+	}
+	showVector (list);
+	
+
 	return true;
-}
+} 
